f3ff31afd0652962edac03d0dc1073a6b9954460
Darmian407 <damianmesa407@gmail.com>
2021-05-08 11:36:31 -0300

    1

--- framework-4.1.1/app/Config/Database.php
+++ framework-4.1.1/app/Config/Database.php
@@ -59,8 +59,8 @@ class Database extends Config
 	public $tests = [
 		'DSN'      => '',
 		'hostname' => '127.0.0.1',
-		'username' => '',
-		'password' => '',
+		'username' => 'root',
+		'password' => 'root',
 		'database' => ':memory:',
 		'DBDriver' => 'SQLite3',
 		'DBPrefix' => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
@@ -73,7 +73,7 @@ class Database extends Config
 		'compress' => false,
 		'strictOn' => false,
 		'failover' => [],
-		'port'     => 3306,
+		'port'     => 3307,
 	];
 
 	//--------------------------------------------------------------------
