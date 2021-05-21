417705bf739268f2fd984a8f8220d8a841821415
Oci-6 <mauri3418@gmail.com>
2021-05-21 15:24:42 -0300

    Modelo Recurso y Controller

--- Codeigniter/app/Controllers/Resource.php
+++ Codeigniter/app/Controllers/Resource.php
@@ -10,4 +10,76 @@ class Resource extends BaseController {
     public function createResource() {
         return view('Resources/createResource');
     }
+
+    public function receiveData() {
+        // Validaton Service
+        $validation =  \Config\Services::validation();
+
+        $rules = [
+            'tipo' => [
+                'label' => 'Tipo',
+                'rules' => 'required'
+            ],
+
+            'descargable' => [
+                'label' => 'Descargable',
+            ],
+
+            'imagen' => [
+                'label' => 'Imagen',
+                'rules' => 'required|valid_url'
+            ],
+
+            'nombre' => [
+                'label' => 'Nombre',
+                'rules' => 'required|alpha_numeric_space'
+            ],
+
+            'descripcion' => [
+                'label' => 'Descripción',
+                'rules' => 'required|alpha_numeric_space'
+            ],
+
+        ];
+
+        if (!$this->validate($rules)) {
+
+            $data = [
+                'errors' => $validation->getErrors()
+            ];
+
+            return view('Resources/createResource', $data);
+
+        }else {
+             // Session Service
+             $session = \Config\Services::session();
+             $user = $session->get('user');
+
+
+             // Request Service
+             $request = service('request');
+ 
+             // Get parametters from http request
+             $tipo = $request->getVar('tipo');
+             $descargable = $request->getVar('descargable');
+             $imagen = $request->getVar('imagen');
+             $nombre = $request->getVar('nombre');
+             $descripcion = $request->getVar('descripcion');
+
+             // Bring model to the controller
+            $resourceModel = new \App\Models\ResourceModel();
+
+            $data = [
+                'tipo' => $tipo,
+                'descargable' => $descargable,
+                'imagen' => $imagen,
+                'nombre' => $nombre,
+                'descripcion' => $descripcion,
+                'autor' => $user['nick']
+            ]; 
+
+            $resourceModel->insert($data);
+        }
+    }
+
 }
---
+++ Codeigniter/app/Models/ResourceModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ResourceModel extends Model
+{
+    // Specifies the database table that this model primarily works with.
+    protected $table = 'recurso';
+
+    // This is the name of the column that uniquely identifies the records in this table.
+    protected $primaryKey = 'numero';
+
+    // This setting allows you to define the type of data that is returned.
+    protected $returnType = 'array';
+
+}
