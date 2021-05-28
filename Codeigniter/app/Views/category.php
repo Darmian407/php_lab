b664d6bec86a1a749b635ba38a54bf1b6a1d2997
Agus458 <aguspe458@outlook.com>
2021-05-28 10:17:41 -0300

    select de categorias

--- Codeigniter/app/Views/category.php
+++ Codeigniter/app/Views/category.php
@@ -7,10 +7,12 @@
 <?php
     function printChildCategories($categories){
         return array_map(function ($category){
-            return '<li><a href="/category/' . $category['id'] . '">' . $category['name'] . '</a><ul>' . implode(" ", printChildCategories($category['child'])) . '</ul></li>';
+            return '<li><a onclick="myFunction(event)" id="' . $category['id'] . '" href="/category/' . $category['id'] . '">' . $category['name'] . '</a><ul>' . implode(" ", printChildCategories($category['child'])) . '</ul></li>';
         }, $categories);
     }
 ?>
+
+<select name="categories[]" class="uk-select uk-margin-small uk-border-rounded" multiple="multiple" id="categories" disabled></select>
 
 <div class="uk-width-1-2@s uk-width-2-5@m uk-height-viewport">
     <ul class="uk-nav-default uk-nav-parent-icon" uk-nav>
@@ -18,7 +20,7 @@
         foreach ($categories as $category) {
         ?>
             <li>
-                <a href="/category/<?= $category['id'] ?>"><?= $category['name'] ?></a>
+                <a onclick="myFunction(event)" id="<?= $category['id'] ?>" href="/category/<?= $category['id'] ?>"><?= $category['name'] ?></a>
                 <ul class="uk-nav-sub">
                     <?= implode(" ", printChildCategories($category['child'])) ?>
                 </ul>
@@ -28,6 +30,28 @@
         ?>
     </ul>
 </div>
+
+<script>
+    let cats = [];
+
+    function myFunction (event) {
+        event.preventDefault();
+        let name = event.target.innerHTML;
+        let id = event.target.id;
+        let categories = document.getElementById('categories');
+
+        if(cats.find(category => category.id == id)){
+            cats = cats.filter(category => category.id != id);
+        } else {
+            cats.push({id, name});
+        }
+        
+        categories.innerHTML = "";
+        cats.forEach(category => {
+            categories.innerHTML += `<option selected value="${category.id}">${category.name}</option>`;
+        });
+    }
+</script>
 
 <!-- Footer Template -->
 <?php echo view('templates/footer') ?>
